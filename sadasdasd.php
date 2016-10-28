4cdc9be213406a8f1a4f4d9d2707916843b2b42a
SulGround <kamil.granieczny@gmail.com>
2016-10-28 10:40:44 +0200

    Add files via upload

---
+++ sadasdasd.php
@@ -0,0 +1,18 @@
+<?php
+$x=rand(-5,15);
+/*
+if($x<0 || $x>=9)
+	print("x nie należy do przedziału");
+else
+	print("x należy do przedzuiału");
+*/
+if($x<0)
+print("nie należy")
+
+else if($x>=9)
+		print("liczba nie należy do przedziału");
+
+else
+	print("należy");
+
+?>
